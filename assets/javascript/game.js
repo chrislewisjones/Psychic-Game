43d0e0761b816e271c159bfc21f78e1fcf3c9e84
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-03-03 23:41:36 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 // first set the variables and an alphabet for the computer to choose from
 
 var guess;
+var guesses = [];
 var wins = 0;
 var losses = 0;
-var guesses = [];
 var lives = 9;
 var computer = [
   "a",
@@ -35,40 +35,48 @@ var computer = [
 ];
 var computerChoice;
 
-//Computer to make a choice:
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var livesText = document.getElementById("lives");
+var guessesText = document.getElementById("guesses");
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+livesText.textContent = "Guesses left: " + lives;
+guessesText.textContent = "Your Guesses so far:" + guesses;
 
-var computerChoice = computer[Math.floor(Math.random() * computer.length)];
-console.log(computerChoice);
+//Start the game with the computer making a choice, also this function is used to restart the game
 
-// user then makes their guesses
+function start() {
+  computerChoice = computer[Math.floor(Math.random() * computer.length)];
+  console.log("comp " + computerChoice);
+  guesses.length = 0;
+  lives = 10;
+}
+start();
+
+// user then makes their guesses running through if else statement, adding to a string of guesses while counting and updating the stats
 
 document.onkeydown = function(event) {
   var guess = event.key;
   console.log(guess);
   guesses.push(guess);
   console.log(guesses);
+  guessesText.textContent = "Your Guesses so far:" + guesses;
+
   if (computerChoice == guess) {
     console.log("Win");
     wins++;
-    console.log(wins);
-    lives = 9;
-    guesses.length = 0;
-  } else if (lives == 0) {
+    console.log("Num of wins " + wins);
+    winsText.textContent = "Wins: " + wins;
+    start();
+  } else if (lives == 1) {
     console.log("final wrong");
     losses++;
-    console.log(losses);
-    lives = 9;
-    guesses.length = 0;
+    console.log("Num of loses " + losses);
+    lossesText.textContent = "Losses: " + losses;
+    start();
   } else console.log("wrong");
   lives--;
-  console.log(lives);
+  console.log("Num of lives " + lives);
+  livesText.textContent = "Guesses left: " + lives;
 };
-
-var one = document.getElementById("wins");
-one.textContent = wins;
-var two = document.getElementById("losses");
-two.textContent = losses;
-var three = document.getElementById("lives");
-three.textContent = lives;
-var four = document.getElementById("guesses");
-four.textContent = guesses;
